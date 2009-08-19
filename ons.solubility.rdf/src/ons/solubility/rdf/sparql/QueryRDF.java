87482f3c2f82d18592de4c65141bfb3f963f7627
Egon Willighagen <egon.willighagen@gmail.com>
2009-08-19 16:07:40 +0200

    Updated for Jena 2.6

--- ons.solubility.rdf/src/ons/solubility/rdf/sparql/QueryRDF.java
+++ ons.solubility.rdf/src/ons/solubility/rdf/sparql/QueryRDF.java
@@ -4,14 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-import com.hp.hpl.jena.query.Query;
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QueryFactory;
-import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.graph.query.Query;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.vocabulary.ResultSet;
 
 public class QueryRDF {
 
